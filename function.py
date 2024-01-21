6d659680f74c71e48686c43959d45e7bc08a0a11
SHAHINUR <shahinuralam.cse@gmail.com>
2024-01-20 21:58:54 -0500

    Function Example

---
+++ function.py
@@ -0,0 +1,8 @@
+# This program is an example of function in python
+   
+#Function Definition
+def multiply_function(x,y):
+    z=x*y
+    return z
+    
+print(multiply_function(5,10)) #calling a function
