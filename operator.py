2183f2275b8cea657b87542fbf81aaebf4a8b73c
SHAHINUR <shahinuralam.cse@gmail.com>
2024-01-21 20:44:39 -0500

    Create operator.py

---
+++ operator.py
@@ -0,0 +1,12 @@
+# This program is an example of an arithmetic operator in python
+ 
+a=14
+b=5
+ 
+print (a+b) # যোগ
+print(a-b) #বিয়োগ
+print(a*b) #গুন
+print(a**b) #exponential 
+print(a/b) #ভাগ
+print(a//b) #ফ্লোর ডিভিশন
+print(a%b) #মডুলাস
