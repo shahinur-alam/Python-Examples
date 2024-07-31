9baacfbb91d424af9c6767d3d1fa5f5dafd2cd93
SHAHINUR <shahinuralam.cse@gmail.com>
2024-07-31 11:33:39 -0400

    Create mutable and immutable.py

---
+++ mutable and immutable.py
@@ -0,0 +1,20 @@
+#Mutable objects can be modified or changed after their creation but the immutable objects can not be modified after their creation. 
+
+#Strings and tuples are immutable, while lists, dictionaries, and sets are mutable. 
+
+lst=[1,2,3] #List
+
+dct={'Name':'Shahinur', 'number':[5,6,7,8]} #dictionary
+
+setexample=([20,22, 'shahinur', 'anything'],[2,3]) #set
+
+strexample='Shahinur' #String
+
+tpl=(10,11,23) #Tuple
+
+print(lst)
+print(dct)
+print(tpl)
+
+print(strexample[1])
+print(setexample)
