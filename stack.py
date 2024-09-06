7fb360feacd712721d6dc8110d9d03dd308089dc
SHAHINUR <shahinuralam.cse@gmail.com>
2024-09-06 17:11:39 -0400

    Stack using built in python method

--- stack.py
+++ stack.py
@@ -6,3 +6,19 @@ Created on Fri Sep  6 16:57:12 2024
 @description: This is an example of stack in Python
 """
 
+#This is a stack implementation using append() and pop() method
+
+stack_var=[10,20,30]
+
+stack_var.append(40) #push element, Python does not have push method
+stack_var.append(50)
+
+print("Stack after appending",stack_var)
+
+stack_var.pop()
+
+print("Stack after pop",stack_var)
+
+stack_var.pop()
+
+print("Stack after pop",stack_var)
