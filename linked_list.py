8595ed1582d0534f53888f31930e41d7358476e8
SHAHINUR <shahinuralam.cse@gmail.com>
2024-09-07 20:29:51 -0400

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -29,10 +29,57 @@ class LinkedList:
             last.next=new_node      #Link the last node to the new node
             
             
+    #Insert a node at the beginning
+    def append_at_begin(self,data):
+        new_node=Node(data)
+        new_node.next=self.head 
+        self.head=new_node
+    
+    #Display Linked List Items
+    def display(self):
+        current=self.head
+        while current:
+            print (current.data, end=" ->")
+            current=current.next
+        print("None")
+        
+    def delete(self, key):
+
+        #Deleting the head
+        if self.head and self.head.data==key:
+            self.head=self.head.next
+            return
+        
+        #if the key is not the first element
+        current=self.head
+        prev=None
+        if current and current.data!=key:
+            prev=current
+            current=current.next
             
+            
+        #if the key is not in the list
+        if current is None:
+            print("Data not in the list")
+            return         
+         
+            
+        prev.next=current.next #Unlink the node
             
 lList=LinkedList()
 
 lList.append(10)
 lList.append(20)
 lList.append(30)
+
+lList.append_at_begin(40)
+
+lList.display()
+
+lList.delete(40)
+
+lList.display()
+
+lList.delete(20)
+
+lList.display()
